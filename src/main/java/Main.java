90d90eedbaac5a13032e60f128dfff2cbd1b9376
TealStone <ultratealstone@gmail.com>
2020-11-24 18:41:01 -0500

    Changed some more things? Github what is happening

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
 
-public class Main {
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException, IOException, ClassNotFoundException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        builder.setToken(Constants.TOKEN);
+        builder.addEventListeners(new Main(), new Dice(), new Misc(), new PuzzleHunt(), new Perversity());
+        builder.build();
+    }
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println("received message: " + event.getMessage().getContentRaw());
+    }
+
 }
